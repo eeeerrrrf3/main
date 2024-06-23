0648f58d3b9fa4b9f31a879ab4e1711ab2e51ac1
Topik-Hub <108158864+eeeerrrrf3@users.noreply.github.com>
2024-06-23 08:08:24 +0300

    Update script.lua

--- Arsenal/war tycoon/script.lua
+++ Arsenal/war tycoon/script.lua
@@ -1,9 +1,9 @@
 
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
-local PhantomForcesWindow = Library:NewWindow("TopikHub|War tycoon")
+local PhantomForcesWindow = Library:NewWindow("TopikHub|Wartycoon")
 
-local KillingCheats = PhantomForcesWindow:NewSection("Combat")
+local KillingCheats = PhantomForcesWindow:NewSection("scripts")
 
 KillingCheats:CreateButton("Fly😊", function()
     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
@@ -114,6 +114,8 @@ end
 end)
 end)
 
+local KillingCheats = PhantomForcesWindow:NewSection("Teleport")
+
 KillingCheats:CreateButton("Tp To Airdrops😊", function()
     local Folder = workspace["Game Systems"]
     local player = game.Players.LocalPlayer.Character.HumanoidRootPart
@@ -125,6 +127,8 @@ KillingCheats:CreateButton("Tp To Airdrops😊", function()
     end
     end
 end)
+
+local KillingCheats = PhantomForcesWindow:NewSection("Kill tool")
 
 KillingCheats:CreateButton("Kill All😊", function()
     function getNil(name,class) for _,v in next, getnilinstances() do if v.ClassName==class and v.Name==name then return v;end end end
@@ -215,7 +219,7 @@ KillingCheats:CreateToggle("Auto Collect", function(value)
             else
                 game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(CashCollectorPart)
             end
-        end (value)
+        end 
 end)
 
 KillingCheats:CreateDropdown("DropDown", {"Hello", "World", "Hello World"}, 2, function(text)
